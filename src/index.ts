20a469194c4eba52c14d081a164ecb5aed322a41
Flouse <1297478+Flouse@users.noreply.github.com>
2025-03-12 03:09:23 +0000

    feat: add multiaddr dependency and implement graph node fetching with peer connection logic

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,26 @@ console.log(
 );
 
 
+import { getGraphNodes } from "./peers";
 import { FiberRPC } from "./rpc/client";
 const rpc = new FiberRPC("http://localhost:58227");
 
 // call get node info
 const nodeInfo = await rpc.getNodeInfo();
 console.log("Node Info:", nodeInfo);
+console.log("Peers Count:", nodeInfo.peers_count);
+
+// connect to all the peers from the graph
+const graphNodes = await getGraphNodes();
+console.log("Graph Nodes:", graphNodes);
+
+graphNodes.forEach((node) => {
+  const multiAddr = node.addresses;
+  multiAddr.forEach((addr) => {
+    rpc.connect_peer(addr, true);
+  });
+});
+
+// check peers count again
+const updatedNodeInfo = await rpc.getNodeInfo();
+console.log("Updated Peers Count:", updatedNodeInfo.peers_count);
---
+++ src/types/fiber.d.ts
@@ -0,0 +1,36 @@
+declare module 'fiber' {
+  // export interface IFiberApplication {
+  //   listen(port: number): void;
+  //   use(middleware: IMiddleware): void;
+  //   get(path: string, handler: IRouteHandler): void;
+  //   post(path: string, handler: IRouteHandler): void;
+  //   put(path: string, handler: IRouteHandler): void;
+  //   delete(path: string, handler: IRouteHandler): void;
+  // }
+
+  export interface IContext {
+    params: { [key: string]: string };
+    query: { [key: string]: string };
+    body: any;
+    headers: { [key: string]: string };
+    status(code: number): IContext;
+    json(data: any): void;
+    send(data: string): void;
+  }
+
+  export interface MultiAddr {
+    id: string;
+    protocol: string;
+    address: string;
+    port?: number;
+  }
+
+  export interface GraphNode {
+    addresses: Array<string> // TODO: Array<MultiAddr>;
+    node_id: string
+    timestamp: string
+    chain_hash: string
+    auto_accept_min_ckb_funding_amount: string
+    udt_cfg_info: Array<UdtCfgInfo>
+  }
+}
